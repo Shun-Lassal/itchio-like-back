8ca00bc8eb41a861fbf173e5f09486d3ae25fecc
Thomas N <thomas.nadi.pro@outlook.fr>
2023-02-01 15:19:30 +0100

    eslint fuckry fuck

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+
+  extends: ["eslint:recommended", "prettier"],
+  rules: {},
+};
